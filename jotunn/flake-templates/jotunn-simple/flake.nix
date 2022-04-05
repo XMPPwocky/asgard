28b640cef0a678127f2a4d9e3b6fab53fdcaac10
Mimir <mimir@xmppwocky.net>
2022-04-05 12:17:13 -0700

    xx

--- jotunn/flake-templates/jotunn-simple/flake.nix
+++ jotunn/flake-templates/jotunn-simple/flake.nix
@@ -3,11 +3,7 @@
 
   outputs = { self, nixpkgs, flake-utils }: let
     packageName = "jotunn-example";
-  {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+  in
     (flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
       thisPackage = pkgs.callPackage (./default.nix) { };
@@ -15,9 +11,6 @@
       packages = {
         ${packageName} = thisPackage;
       };
-      defaultPackage = thisPackage;
-    };
-
-
-  };
+      packages.default = thisPackage;
+    }));
 }
