a34a6165fba32be35e0368e8eef1464d3e90c73c
Mimir <mimir@xmppwocky.net>
2022-04-05 11:03:40 -0700

    done

--- flake.nix
+++ flake.nix
@@ -4,5 +4,13 @@
   outputs = { self, nixpkgs, flake-utils }:
   {
     nixosModules = import ./nixos/modules/default.nix;
-  };
+  } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+        packages.jack-patches = pkgs.fetchurl {
+          name = "jack-patches";
+          url = "file://${self}/jack_patches";
+        };
+    });
 }
